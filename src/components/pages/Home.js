ed78d803f061c4dca8524277642c03a0a619c391
alok-karn <alokkyasth@gmail.com>
2023-05-26 23:34:13 +0530

    second commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,33 +1,31 @@
-import React, { useEffect } from 'react'
-import Header from '../imports/Header'
-import { useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import React, { useEffect } from "react";
+import Header from "../imports/Header";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 export default function Home() {
-  const user = useSelector(state => state.isLoggedIn)
-  const history = useHistory()
+    const user = useSelector((state) => state.isLoggedIn);
+    const history = useHistory();
 
-  const route = () => {
-    const token = localStorage.getItem('x-access-token')
-    return token ? true : false
-  }
+    const route = () => {
+        const token = localStorage.getItem("x-access-token");
+        return token ? true : false;
+    };
 
-  useEffect(() => {
-    if (!route()) {
-      history.push('/login')
-    }
-
-   },[route, history])
-  //  console.log(route())
-  //  console.log(user)
-  return (
-      <>
-          <Header></Header>
-          <main>
-          <div className="container">
-                <h4>Welcome to Home Page</h4>
-          </div>
-          </main>
-       
-    </>
-  )
+    useEffect(() => {
+        if (!route()) {
+            history.push("/login");
+        }
+    }, [route, history]);
+    //  console.log(route())
+    //  console.log(user)
+    return (
+        <>
+            <Header></Header>
+            <main>
+                <div className="container">
+                    <h4>Welcome to Home Page</h4>
+                </div>
+            </main>
+        </>
+    );
 }
